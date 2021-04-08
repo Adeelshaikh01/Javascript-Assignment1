6937d3b195c51ca5f0588d1a279b37e9810411d1
Muhammad Adeel <adeelshaikh654@gmail.com>
2021-04-08 17:49:43 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// Write a script to greet your website visitor using JS alert box.
+alert("Welcome to Our Website");
+
+// Write a script to display following message on your web page
+alert("Hello, Mr.Bean \nHow are you?");
+
+// Variables for Strings
+
+var message = "Hello World";
+alert(message)
+
+var book = "A smarter way to learn JavaScript";
+document.write(book)
+document.write("<br>");
+
+// Variables for Number
+
+var age = 23;
+alert("Age=" + age);
+
+var birthYear = "24-12-1997";
+document.write("D.O.B = " + birthYear)
